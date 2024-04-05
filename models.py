70dab1a80cc043ff2e21d4e086ade3c440ed5940
Kyamran Rizayev <kyamranr@gmail.com>
2024-04-05 13:37:26 -0400

    created models and cupcakes database

--- models.py
+++ models.py
@@ -1,1 +1,22 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database"""
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Coumn(db.Text, nullable=False)
+    size = db.Coumn(db.Text, nullable=False)
+    rating = db.Coumn(db.Float, nullable=False)
+    image = db.Coumn(db.Text, default='https://tinyurl.com/demo-cupcake')
