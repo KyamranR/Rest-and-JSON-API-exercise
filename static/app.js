9c5d9b5781630ab6c905190eefdde93591e49f11
Kyamran Rizayev <kyamranr@gmail.com>
2024-04-08 21:46:28 -0500

    added app.js

---
+++ static/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  function loadCupcakes() {
+    axios
+      .get("/api/cupcakes")
+      .then(function (response) {
+        $("#cupcakes-list").empty();
+        response.data.cupcakes.forEach((cupcake) => {
+          $("#cupcakes-list").append(`
+                        <div>
+                            <p>Flavor: ${cupcake.flavor}</p>
+                            <p>Size: ${cupcake.size}</p>
+                            <p>Rating: ${cupcake.rating}</p>
+                            <img src="${cupcake.image}" alt="Cupcake Image">
+                        </div>
+                    `);
+        });
+      })
+      .catch(function (error) {
+        console.error("Error loading cupcakes: ", error);
+      });
+  }
+
+  loadCupcakes();
+
+  $("#cupcake-form").submit(function (event) {
+    event.preventDefault();
+    const flavor = $("#flavor").val();
+    const size = $("#size").val();
+    const rating = $("#rating").val();
+    const image = $("#image").val();
+
+    axios
+      .post("/api/cupcakes", {
+        flavor: flavor,
+        size: size,
+        rating: rating,
+        image: image,
+      })
+      .then(function (response) {
+        console.log("Cupcakes added:", response.data);
+        loadCupcakes();
+      })
+      .catch(function (error) {
+        console.error("Error adding cupcake: ", error);
+      });
+  });
+});
